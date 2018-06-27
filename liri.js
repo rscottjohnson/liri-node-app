8440fa15d90913e99cccbdcf05c2d9224714b0f0
Scott Johnson <sjwvuie@yahoo.com>
2018-06-27 16:33:12 -0400

    Updated Twitter and Spotify

--- liri.js
+++ liri.js
@@ -1,35 +1,109 @@
 // Access the .env file
 require("dotenv").config();
 
+// VARIABLES
+//====================
+
 // Access the keys.js file and set it to a variable
 var keys = require("./keys.js");
+
+// Require the npm pakages
 var inquirer = require("inquirer");
-var twitter = require("twitter");
+var moment = require("moment");
+var Twitter = require("twitter");
+var Spotify = require('node-spotify-api');
+var request = require("request"); // OMDB API
 
-// Access the keys information
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// FUNCTIONS
+//====================
 
-var inputStr = process.argv;
-var action = inputStr[2];
+function tweets() {
+  var client = new Twitter(keys.twitter);
+
+  var params = {
+    screen_name: 'FauxScoJo',
+    count: 20
+  };
+  client.get('statuses/user_timeline', params, function (error, tweets, response) {
+    if (error) {
+      console.log(error);
+    }
+    console.log("\n===============================\n")
+    console.log("Here is a listing of tweets:")
+    console.log("\n===============================\n")
+    for (var i = 0; i < params.count; i++) {
+      console.log((i + 1) + ". Created: " + moment(tweets[i].created_at, "dd MMM DD HH:mm:ss ZZ YYYY", "en").format("MMMM Do YYYY, h:mm a") + " | " + tweets[i].text);
+    }
+  });
+}
+
+function spotThisSong() {
+  inquirer.prompt([{
+        type: "input",
+        message: "Type the name of the song you'd like to Spotify.",
+        name: "songName"
+      },
+      {
+        type: "confirm",
+        message: "Are you sure:",
+        name: "confirm",
+        default: true
+      }
+    ])
+    .then(function (songAns) {
+      if (songAns.confirm) {
+
+        var spotify = new Spotify(keys.spotify);
+
+        // Need to add something to default to "The Sign" if no song name is provided
+
+        spotify.search({
+          type: 'track',
+          query: songAns.songName,
+          limit: 1
+        }, function (err, data) {
+          if (err) {
+            return console.log('Error occurred: ' + err);
+          }
+          // console.log(JSON.stringify(data, null, 2));
+          console.log("\n===============================\n")
+          console.log("Song information from Spotify:")
+          console.log("\n===============================\n")
+          console.log("ARTIST: " + data.tracks.items[0].artists[0].name);
+          console.log("SONG TITLE: " + data.tracks.items[0].name);
+          console.log("PREVIEW LINK: " + data.tracks.items[0].preview_url);
+          console.log("ALBUM NAME: " + data.tracks.items[0].album.name);
+        });
+      } else {
+        console.log("\nThat's okay.  Come again when you are more sure.\n");
+      }
+    });
+}
+
+function omdb() {
+
+}
+
+// MAIN PROCESS
+//====================
 
 inquirer.prompt([{
     type: "checkbox",
-    name: "liriAction",
     message: "Which action would you like to take?",
-    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+    choices: ["Tweets", "Spotify", "Movies", "Do what it says"],
+    name: "liriAction"
   }])
-  .then(function (inquirerResponse) {
-    if (inquirerResponse.liriAction === "my-tweets") {
-      console.log("my-tweets was picked");
-    } else if (inquirerResponse.liriAction === "spotify-this-song") {
-      console.log("spotify-this-song was picked");
-    } else if (inquirerResponse.liriAction === "movie-this") {
+  .then(function (user) {
+    if (user.liriAction == "Tweets") {
+      tweets();
+    } else if (user.liriAction == "Spotify") {
+      spotThisSong();
+    } else if (user.liriAction == "movie-this") {
       console.log("movie-this was picked");
-    } else if (inquirerResponse.liriAction === "do-what-it-says") {
+    } else if (user.liriAction == "do-what-it-says") {
       console.log("do-what-it-says was picked");
     } else {
-      console.log("Ok. Come back when you'd like to pick something awesome.")
+      console.log("Ok. Come back when you'd like to pick something awesome.");
     }
   });
 
