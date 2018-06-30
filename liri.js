177cb8f8fb4bae5f7bea769f819a1e732b7ed01e
Scott Johnson <sjwvuie@yahoo.com>
2018-06-29 23:56:57 -0400

    Updated liri.js

--- liri.js
+++ liri.js
@@ -11,7 +11,7 @@ var keys = require("./keys.js");
 var inquirer = require("inquirer");
 var moment = require("moment");
 var Twitter = require("twitter");
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 var request = require("request"); // OMDB API
 var fs = require("fs"); // reading / writing files
 
@@ -21,7 +21,8 @@ var songSearch = "";
 var movieSearch = "";
 var dataArr = [];
 var textFile = "log.txt";
-var divider = "\n\n============================================================\n\n";
+var divider =
+  "\n\n============================================================\n\n";
 
 // FUNCTIONS
 //====================
@@ -31,28 +32,40 @@ function tweets() {
   var client = new Twitter(keys.twitter);
 
   var params = {
-    screen_name: 'FauxScoJo',
+    screen_name: "FauxScoJo",
     count: 20
   };
-  client.get('statuses/user_timeline', params, function (error, tweets, response) {
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
     if (error) {
       console.log(error);
     }
-    console.log("\n===============================\n")
-    console.log("Here is a listing of tweets:")
-    console.log("\n===============================\n")
     for (var i = 0; i < params.count; i++) {
-      console.log((i + 1) + ". Created: " + moment(tweets[i].created_at, "dd MMM DD HH:mm:ss ZZ YYYY", "en").format("MMMM Do YYYY, h:mm a") + " | " + tweets[i].text);
+      var tweetData = [
+        i + 1 + ". Created: " + moment(tweets[i].created_at,"dd MMM DD HH:mm:ss ZZ YYYY","en").format("MMMM Do YYYY, h:mm a") + " | " + tweets[i].text,
+        divider
+      ].join("\n\n");
+
+      // Log the data to the textFile
+      fs.appendFile(textFile, tweetData, function(err) {
+        if (err) throw err;
+      });
+      console.log(tweetData);
     }
   });
 }
 
+// Spotify search
 function spotThisSong() {
-
   var spotify = new Spotify(keys.spotify);
-
+  // If the user didn't choose 'do what it says', prompt for info
   if (dwis === false) {
-    inquirer.prompt([{
+    inquirer
+      .prompt([
+        {
           type: "input",
           message: "Type the name of the song: ",
           name: "songName"
@@ -64,40 +77,49 @@ function spotThisSong() {
           default: true
         }
       ])
-      .then(function (songAns) {
+      .then(function(songAns) {
+        // If the user confirmed 'yes'
         if (songAns.confirm) {
-
+          // If the user didn't enter a song
           if (songAns.songName.length < 1) {
             songSearch = "Ace of Base The Sign";
           } else {
             songSearch = songAns.songName;
           }
-          spotify.search({
-            type: 'track',
-            query: songSearch,
-          }, function (err, data) {
-            if (err) {
-              return console.log('Error occurred: ' + err);
+          spotify.search(
+            {
+              type: "track",
+              query: songSearch
+            },
+            function(err, data) {
+              if (err) {
+                return console.log("Error occurred: " + err);
+              }
+              for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = [
+                  "ARTIST: " + data.tracks.items[i].artists[0].name,
+                  "SONG TITLE: " + data.tracks.items[i].name,
+                  "PREVIEW LINK: " + data.tracks.items[i].preview_url,
+                  "ALBUM NAME: " + data.tracks.items[i].album.name,
+                  divider
+                ].join("\n\n");
+
+                // Log the data to the textFile
+                fs.appendFile(textFile, songData, function(err) {
+                  if (err) throw err;
+                });
+                console.log(songData);
+              }
             }
-            for (var i = 0; i < data.tracks.items.length; i++) {
-              console.log("\n===============================\n")
-              console.log("Song information from Spotify:")
-              console.log("\n===============================\n")
-              console.log("ARTIST: " + data.tracks.items[i].artists[0].name);
-              console.log("SONG TITLE: " + data.tracks.items[i].name);
-              console.log("PREVIEW LINK: " + data.tracks.items[i].preview_url);
-              console.log("ALBUM NAME: " + data.tracks.items[i].album.name);
-            }
-            console.log("\n==========END OF RESULTS=========\n")
-          });
+          );
+          // If the user confirmed 'no'
         } else {
           console.log("\nThat's okay.  Come again when you are more sure.\n");
         }
       });
   } else {
     // Read in the data from random.txt
-    fs.readFile("random.txt", "utf8", function (error, data) {
-
+    fs.readFile("random.txt", "utf8", function(error, data) {
       if (error) {
         return console.log(error);
       }
@@ -107,33 +129,43 @@ function spotThisSong() {
 
       songSearch = dataArr[1];
 
-      spotify.search({
-        type: 'track',
-        query: songSearch
-      }, function (err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
+      spotify.search(
+        {
+          type: "track",
+          query: songSearch
+        },
+        function(err, data) {
+          if (err) {
+            return console.log("Error occurred: " + err);
+          }
+          for (var i = 0; i < data.tracks.items.length; i++) {
+            var songData = [
+              "ARTIST: " + data.tracks.items[i].artists[0].name,
+              "SONG TITLE: " + data.tracks.items[i].name,
+              "PREVIEW LINK: " + data.tracks.items[i].preview_url,
+              "ALBUM NAME: " + data.tracks.items[i].album.name,
+              divider
+            ].join("\n\n");
+
+            // Log the data to the textFile
+            fs.appendFile(textFile, songData, function(err) {
+              if (err) throw err;
+            });
+            console.log(songData);
+          }
         }
-        for (var i = 0; i < data.tracks.items.length; i++) {
-          console.log("\n===============================\n")
-          console.log("Song information from Spotify:")
-          console.log("\n===============================\n")
-          console.log("ARTIST: " + data.tracks.items[i].artists[0].name);
-          console.log("SONG TITLE: " + data.tracks.items[i].name);
-          console.log("PREVIEW LINK: " + data.tracks.items[i].preview_url);
-          console.log("ALBUM NAME: " + data.tracks.items[i].album.name);
-        }
-        console.log("\n==========END OF RESULTS=========\n")
-      });
+      );
     });
   }
 }
 
 // Movie search
 function omdb() {
-  // If the user didn't choose 'do what it says' prompt for info
+  // If the user didn't choose 'do what it says', prompt for info
   if (dwis === false) {
-    inquirer.prompt([{
+    inquirer
+      .prompt([
+        {
           type: "input",
           message: "Type the name of the movie: ",
           name: "movieName"
@@ -145,10 +177,9 @@ function omdb() {
           default: true
         }
       ])
-      .then(function (movieAns) {
+      .then(function(movieAns) {
         // If the user confirmed 'yes'
         if (movieAns.confirm) {
-
           // If the user didn't enter a movie
           if (movieAns.movieName.length < 1) {
             movieSearch = "Mr. Nobody";
@@ -157,12 +188,14 @@ function omdb() {
           }
 
           // Request to the OMDB API
-          var queryUrl = "http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+          var queryUrl =
+            "http://www.omdbapi.com/?t=" +
+            movieSearch +
+            "&y=&plot=short&tomatoes=true&apikey=trilogy";
 
-          request(queryUrl, function (error, response, body) {
+          request(queryUrl, function(error, response, body) {
             // If the request is successful
             if (!error && response.statusCode === 200) {
-              
               var movieData = [
                 "MOVIE TITLE: " + movieSearch,
                 "RELEASE YEAR: " + JSON.parse(body).Year,
@@ -172,26 +205,26 @@ function omdb() {
                 "LANGUAGE: " + JSON.parse(body).Language,
                 "PLOT: " + JSON.parse(body).Plot,
                 "ACTORS: " + JSON.parse(body).Actors,
+                divider
               ].join("\n\n");
-    
+
               // Log the data to the textFile
-              fs.appendFile(textFile, movieData + divider, function(err) {
+              fs.appendFile(textFile, movieData, function(err) {
                 if (err) throw err;
                 console.log(movieData);
               });
             }
           });
 
-        // If the user confirmed 'no'
+          // If the user confirmed 'no'
         } else {
           console.log("\nThat's okay.  Come again when you are more sure.\n");
         }
       });
-  
-  // If the user chose 'do what it says', read random.txt
-  } else {
-    fs.readFile("random.txt", "utf8", function (error, data) {
 
+    // If the user chose 'do what it says', read random.txt
+  } else {
+    fs.readFile("random.txt", "utf8", function(error, data) {
       if (error) {
         return console.log(error);
       }
@@ -200,13 +233,14 @@ function omdb() {
       movieSearch = dataArr[1];
 
       // Request to the OMDB API
-      var queryUrl = "http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+      var queryUrl =
+        "http://www.omdbapi.com/?t=" +
+        movieSearch +
+        "&y=&plot=short&tomatoes=true&apikey=trilogy";
 
-      request(queryUrl, function (error, response, body) {
-
+      request(queryUrl, function(error, response, body) {
         // If the request is successful
         if (!error && response.statusCode === 200) {
-
           var movieData = [
             "MOVIE TITLE: " + movieSearch,
             "RELEASE YEAR: " + JSON.parse(body).Year,
@@ -216,10 +250,11 @@ function omdb() {
             "LANGUAGE: " + JSON.parse(body).Language,
             "PLOT: " + JSON.parse(body).Plot,
             "ACTORS: " + JSON.parse(body).Actors,
+            divider
           ].join("\n\n");
 
           // Log the data to the textFile
-          fs.appendFile(textFile, movieData + divider, function(err) {
+          fs.appendFile(textFile, movieData, function(err) {
             if (err) throw err;
             console.log(movieData);
           });
@@ -229,25 +264,27 @@ function omdb() {
   }
 }
 
+// Do what it says
 function dWIS() {
-  fs.readFile("random.txt", "utf8", function (error, data) {
-
+  fs.readFile("random.txt", "utf8", function(error, data) {
     if (error) {
       return console.log(error);
     }
 
     // Then split it by commas (to make it more readable)
     dataArr = data.split(",");
-    if (dataArr[0] == 'my-tweets') {
+    if (dataArr[0] == "my-tweets") {
       tweets();
-    } else if (dataArr[0] == 'spotify-this-song') {
+    } else if (dataArr[0] == "spotify-this-song") {
       dwis = true;
       spotThisSong();
-    } else if (dataArr[0] == 'movie-this') {
+    } else if (dataArr[0] == "movie-this") {
       dwis = true;
       omdb();
     } else {
-      console.log("Please set the first argument in random.txt to my-tweets, spotify-this-song, or movie-this.");
+      console.log(
+        "Please set the first argument in random.txt to my-tweets, spotify-this-song, or movie-this."
+      );
     }
   });
 }
@@ -255,13 +292,16 @@ function dWIS() {
 // MAIN PROCESS
 //====================
 
-inquirer.prompt([{
-    type: "checkbox",
-    message: "Which action would you like to take?",
-    choices: ["Tweets", "Spotify", "Movie", "Do what it says"],
-    name: "liriAction"
-  }])
-  .then(function (user) {
+inquirer
+  .prompt([
+    {
+      type: "checkbox",
+      message: "Which action would you like to take?",
+      choices: ["Tweets", "Spotify", "Movie", "Do what it says"],
+      name: "liriAction"
+    }
+  ])
+  .then(function(user) {
     if (user.liriAction == "Tweets") {
       tweets();
     } else if (user.liriAction == "Spotify") {
